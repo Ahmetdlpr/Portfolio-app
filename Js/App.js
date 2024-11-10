cb4d69ab84d51bb6c46e5f1637a2351892dc4d18
Ahmetdlpr <mybqadresadrs@outlook.com>
2024-11-10 19:23:14 +0300

    Front-End Updated

--- Js/App.js
+++ Js/App.js
@@ -1,11 +1,40 @@
+/* Tanımlanan öğeler */
 let Loginbtn=document.querySelector(".Loginbtn");
 let Logininput=document.querySelector(".Logininput");
 let Loginpage=document.querySelector(".Loginpage");
 let Logininfo=document.querySelector(".Logininfo");
-Loginpage.remove();
+let appcontainer=document.querySelector(".appcontainer");
+let App=document.querySelector(".App");
+let mainpage=document.querySelector(".mainpage");
+let Profile=document.querySelector(".Profile");
+let profilebtn=document.querySelector("#profilebtn");
+let Username=document.querySelector(".Username");
+let Screencontainer=document.querySelector(".Screencontainer");
+let giriskontrol=false;
+
+
+
+/* Silenen öğeler*/ 
+appcontainer.remove();
+Profile.remove();
+
+
+
+
+/* Tanımlanan değişkenler*/ 
 let karaktersayac=0;
+let username;
+
+
+
+
+
+
+
+
+/* Fonksiyonlar*/
 const Login=()=>{
-let username=Logininput.value;
+usernameinput=Logininput.value;
 
     for(let i=0; i<Logininput.value.length; i++)
     {
@@ -13,39 +42,69 @@ let username=Logininput.value;
         karaktersayac++;
     }
 
-              if(karaktersayac<20)
+
+
+
+             /* Login */
+              if(karaktersayac<20 && karaktersayac!=0)
                 {
+             
+             
                 Loginpage.remove();
-                    
+                App.appendChild(appcontainer);
+                Username.textContent="Kullanıcı adı"+usernameinput;
 
-                           
 
 
+              /* Profile page */ 
+               const Profileadd=()=>{
+                mainpage.remove();
+
+                Screencontainer.appendChild(Profile);
+               }
+             
+
+
+
+
+
+             
+                /*Event listenerlar*/
+                profilebtn.addEventListener("click",Profileadd); 
                 }
 
-        else 
+
+
+
+
+
+
+
+
+
+         /* Login Error*/ 
+         else 
         {
-            Logininfo.textContent="Kullanıcı adınızı girerken 20 karakter kullanabilirsiniz";
-        }
+            Logininfo.textContent="Karakter sınırı aşıldı kullanıcı adınızı girerken 20 karakter kullanabilirsiniz";
+            karaktersayac=0
+            
+         }
+
+
+        /*-----Login Error 2--------*/         
+        if(Logininput.value.length==0)
+        {
+            Logininfo.textContent="Giriş yapmak için kullanıcı adı girmeniz gerekir...";
           
-        if(karaktersayac==0)
-        {
-            Logininfo.textContent="Kullanıcı adı giriniz";
         }
+
+
+
 
 }
 
 
 
 
-
-
-
-
-
-
-
-
-
-
+/* Eventlistenerlar*/
 Loginbtn.addEventListener("click", Login);
